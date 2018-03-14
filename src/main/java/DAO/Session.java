af53d8ed9020e5ccdea9dab7b7db5305d5765cfc
GABRI <gabrigoca@gmail.com>
2018-03-14 17:26:28 +0100

    get

--- src/main/java/DAO/Session.java
+++ src/main/java/DAO/Session.java
@@ -1,4 +1,12 @@
 package DAO;
 
 public class Session {
+    public Object get(int id,Class clase){
+        //String query="SELECT * FROM";
+        StringBuffer query=new StringBuffer("SELECT * FROM");
+        query.append(clase.getName());
+        query.append("WHERE id=");
+        query.append(id);
+
+    }
 }
