a2738ad5497e193a187aaa8599aa9d2b8dda1f59
GABRI <gabrigoca@gmail.com>
2018-03-14 17:46:01 +0100

    log ready

--- src/main/java/DAO/Session4.java
+++ src/main/java/DAO/Session4.java
@@ -1,12 +1,15 @@
 package DAO;
+import org.apache.log4j.Logger;
 
 public class Session4 {
+    final static Logger log = Logger.getLogger(Main.class);
     public Object get(int id,Class clase){
         //String query="SELECT * FROM";
         StringBuffer query=new StringBuffer("SELECT * FROM");
         query.append(clase.getName());
         query.append("WHERE id=");
         query.append(id);
-
+        log.debug("Query:" + query);
+        return null;
     }
 }
