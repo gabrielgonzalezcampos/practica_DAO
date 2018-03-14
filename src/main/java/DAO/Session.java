88b9b3c7885937a79fb35b0586caf1ab0ab2c23e
marcllena <marcllena@hotmail.com>
2018-03-14 17:34:01 +0100

    Afegir Factory i save

--- src/main/java/DAO/Session.java
+++ src/main/java/DAO/Session.java
@@ -1,4 +1,23 @@
 package DAO;
 
+import java.lang.reflect.Field;
+
 public class Session {
+
+    public void save(Object a) {
+        Field camps[]=a.getClass().getDeclaredFields();
+        StringBuffer query = new StringBuffer("INSERT INTO");
+        query.append(a.getClass().getName());
+        query.append(" VALUES (");
+        for(int i=0; i<camps.length-1;i++){
+            query.append("?,");
+        }
+        query.append("?)");
+        //Executem primera query
+        //log.debug("Primera String:" + query);
+        for(int i=0; i<camps.length-1;i++){
+            query= new StringBuffer("UPDATE ");
+        }
+
+    }
 }
